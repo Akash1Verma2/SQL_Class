54587610fba2b5dce1a6423fa12ec4774d898ac1
Akash1Verma2 <ankitverma8621@gmail.com>
2026-03-20 12:21:18 +0530

    Connection to the database

---
+++ index.js
@@ -0,0 +1,35 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    database: 'delta_app',
+    password: 'Akash1Verma2!@#'
+})
+
+//Inserting New Data
+let q = "INSERT INTO user (id, username, email, password) VALUES(?,?,?,?)";
+let user2 = ["123", "123_newuser", "abc@gmail.com", "abc"];
+let user3 = ["123", "123_newuser", "abc@gmail.com", "abc"];
+
+try{
+    connection.query(q, user, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+    })
+}catch (err){
+    console.log(err);
+}
+
+connection.end();
+
+let  getRandomUser= () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
+
